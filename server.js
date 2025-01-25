b6f6c11193261342e9600ec89f08774a9e678568
Zeuch <52895870+Wiktorxd1337@users.noreply.github.com>
2025-01-25 20:06:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/create-checkout-session', async (req, res) => {
 
     // Tworzenie sesji płatności Stripe
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+      payment_method_types: ['card', 'blik'],
       line_items: lineItems,
       mode: 'payment',
       success_url: successUrl,
